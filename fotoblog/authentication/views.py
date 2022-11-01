584c9be0604ea66d78ba8b6824083c39271a6c44
Sirisk <adamchrisk@gmail.com>
2022-11-01 22:04:54 +0200

    authentification-connexion-restriction

--- fotoblog/authentication/views.py
+++ fotoblog/authentication/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from . import forms
 
 # Create your views here.
+
+def login_page(request):
+    form = forms.LoginForm()
+    #message vide au depart
+    message =''
+    if request.method =='POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            # authentifier l'utilisateur
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request,user)
+                message = f'Bonjour,' \
+                        f'{user.username}! Vous êtes connecté.'
+                # connfigurez une redirectuion apres la connexion
+                return redirect('home')
+            else:
+                message = 'Identifiants invalides.'
+    return render(request,
+                  'authentication/login.html', context={'form': form,
+                                                        'message': message}
+                  )
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
--- fotoblog/blog/views.py
+++ fotoblog/blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+#restrindre l accès qu aux utilisateur authentifiés
+@login_required
+def home(request):
+    return render(request,
+                  'blog/home.html'
+                  )
