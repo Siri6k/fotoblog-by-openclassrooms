9e8e07d9d92f0510a8e281b8863128fce730064a
Sirisk <adamchrisk@gmail.com>
2022-11-16 23:43:03 +0200

    stockez des données sur les relation s plusieurs a plusieurs

--- fotoblog/blog/views.py
+++ fotoblog/blog/views.py
@@ -51,6 +51,9 @@ def blog_and_photo_upload(request):
             blog.author = request.user
             blog.photo = photo
             blog.save()
+            blog.contributors.add(request.user,
+                                  through_defaults={'contribution':
+                                                    'Auteur principal'})
             return redirect('home')
 
     context = {
